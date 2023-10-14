16426d99d49c0d855f9d6b7c1840f50f06c6a0b3
JadenAntM <jaden.ay.moore@gmail.com>
2023-10-14 16:47:09 -0400

    Added copy URL, history and results.

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,6 +9,7 @@ const Demo = () => {
   });
 
   const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("");
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
@@ -22,52 +23,119 @@ const Demo = () => {
     }
   }, []);
 
-  if (data?.summary) {
-    const newArticle = { ...article, summary: data.summary };
-    const updatedAllArticles = [newArticle, ...allArticles];
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    setArticle(newArticle);
-    setAllArticles(updatedAllArticles);
+    const { data } = await getSummary({ articleUrl: article.url });
 
-    console.log(newArticle);
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
 
-    localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
-  }
-};
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
 
-return (
-  <section className="mt-16 w-full max-w-xl">
-    {/* Search */}
-    <div className="flex flex-col w-full gap-2">
-      <form
-        className="relative flex justify-center item-center"
-        onSubmit={handleSubmit}
-      >
-        <img
-          src={linkIcon}
-          alt="link_icon"
-          className="absolute left-0 my-2 ml-3 w-5"
-        />
+      console.log(newArticle);
 
-        <input
-          type="url"
-          placeholder="Enter URL"
-          value={article.url}
-          onChange={(e) => setArticle({ ...article, url: e.target.value })}
-          required
-          className="url_input peer"
-        />
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+    }
+  };
 
-        <button
-          type="submit"
-          className="submit_btn 
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      handleSubmit(e);
+    }
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* Search */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center item-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+
+          <input
+            type="url"
+            placeholder="Enter URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn 
             peer-focus:border-gray-700
             peer-focus:text-gray-700"
-        >
-          →
-        </button>
-      </form>
-    </div>
-  </section>
-);
+          >
+            →
+          </button>
+        </form>
+
+        {/* History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                <img
+                  src={copied === item.url ? tick : copy}
+                  alt={copied === item.url ? "tick_icon" : "copy_icon"}
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/*Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Oops! that wasn't supposed to happen...
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue2_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </section>
+  );
+};
 export default Demo;
